8794a4698e6ae32448c8f9b983260a43cd268b3b
Felipe Alves <felipealves0891@gmail.com>
2021-02-01 17:57:47 -0300

    Ajustes e comentarios
    Adicionado outra execução sem a implementação do transformer builder, e comentado o codigo da aplicação

--- src/Circle.MVP/Program.cs
+++ src/Circle.MVP/Program.cs
@@ -13,25 +13,67 @@ namespace Circle.MVP
     {
         static void Main(string[] args)
         {
+            Run();
+            RunWithBuilder();
+        }
+
+        private static void Run() 
+        {
             string path = @"D:\db\";
 
+            // Cria uma stream que sera lido
             var inputStream = new TextInputSource();
             inputStream.Open(Path.Combine(path, "input.csv"));
 
+            // Cria uma stream que sera gravado
             var outputStream = new TextOutputSource();
             outputStream.Open(Path.Combine(path, "output.csv"));
 
-            var builder = new TransformationBuilder();
-            var transformation = builder.SetInput(inputStream)
-                                       .SetOutput(outputStream)
-                                       .AddTransformerByType(typeof(TransformerNullToEmpty))
-                                       .AddTransformerByType(typeof(TransformerToLower))
-                                       .AddRowTransformerByType(typeof(RowTransformerSkipLine), 0)
-                                       .Build();
+            // Cria a transformação
+            var transformation = new Transformation(inputStream, outputStream);
+            transformation.AddRowTransformer(new RowTransformerSkipLine(0));
+            transformation.AddTransformer(new TransformerNullToEmpty());
+            transformation.AddTransformer(new TransformerToLower());
 
+            // Adiciona um evento, com ele podemos acessar os estatos da transformação
             transformation.Diagnose += Transformation_Diagnose;
+
+            // Executa a transformação e finaliza a transformação
             transformation.Execute();
             transformation.Dispose();
+
+        }
+
+        private static void RunWithBuilder() 
+        {
+            string path = @"D:\db\";
+
+            // Cria uma stream que sera lido
+            var inputStream = new TextInputSource();
+            inputStream.Open(Path.Combine(path, "input.csv"));
+
+            // Cria uma stream que sera gravado
+            var outputStream = new TextOutputSource();
+            outputStream.Open(Path.Combine(path, "output.csv"));
+
+            // Cria um builder
+            var builder = new TransformationBuilder();
+
+            // Adiciona os streams e define as transformações que serão adicionadas
+            var transformation = builder.SetInput(inputStream)
+                                        .SetOutput(outputStream)
+                                        .AddTransformerByType(typeof(TransformerNullToEmpty))
+                                        .AddTransformerByType(typeof(TransformerToLower))
+                                        .AddRowTransformerByType(typeof(RowTransformerSkipLine), 0)
+                                        .Build();
+
+            // Adiciona um evento, com ele podemos acessar os estatos da transformação
+            transformation.Diagnose += Transformation_Diagnose;
+
+            // Executa a transformação e finaliza a transformação
+            transformation.Execute();
+            transformation.Dispose();
+
         }
 
         private static void Transformation_Diagnose(object sender, EventArgs e)
