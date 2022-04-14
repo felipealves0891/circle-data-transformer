33a2b841c4f78e45f79cb00823a7a88bd5f6f04b
= <=>
2022-04-14 05:01:51 -0300

    Criado e testado

---
+++ src/Circle.Core/DataTransformer/Transformer/Transformations/TransformerReplaceText.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Circle.Core.DataTransformer.Transformer.Transformations
+{
+
+    public class TransformerReplaceText : ITransformer
+    {
+        private StringBuilder stringBuilder;
+        private readonly string search;
+        private readonly string replacement;
+
+        public TransformerReplaceText(string search, string replacement)
+        {
+            this.search = search;
+            this.replacement = replacement;
+            this.stringBuilder = new StringBuilder();
+        }
+
+        public object Transform(object inputData, int inputIndex, int lineIndex)
+        {
+            stringBuilder = stringBuilder.Clear();
+            stringBuilder = stringBuilder.Append(inputData);
+            stringBuilder = stringBuilder.Replace(search, replacement);
+            return stringBuilder.ToString();
+        }
+
+    }
+}
---
+++ tests/Circle.Core.Tests/DataTransformer/Transformer/Transformations/TransformerReplaceTextTest.cs
@@ -0,0 +1,29 @@
+﻿using Circle.Core.DataTransformer.Transformer;
+using Circle.Core.DataTransformer.Transformer.Transformations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Circle.Core.Tests.DataTransformer.Transformer.Transformations
+{
+    public class TransformerReplaceTextTest
+    {
+        [Fact]
+        public void Transform_Replacement_Assertive()
+        {
+            //Arrange
+            string text = "123|456";
+            string search = "|";
+            string replacement = ";";
+            string expected = "123;456";
+
+            //Act
+            TransformerReplaceText transformer = new TransformerReplaceText(search, replacement);
+
+            //Assert
+            Assert.Equal(expected, transformer.Transform(text, 0, 0));
+        }
+    }
+
+}
