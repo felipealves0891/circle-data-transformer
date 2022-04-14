b51b678943b200962fa075dbb98bb7d4e98218c5
= <=>
2022-04-14 05:04:57 -0300

    Add teste

---
+++ tests/Circle.Core.Tests/DataTransformer/Inputs/Sources/TextInputSourceTest.cs
@@ -0,0 +1,36 @@
+﻿using Circle.Core.DataTransformer.Input.Sources;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace Circle.Core.Tests.DataTransformer.Inputs.Sources
+{
+    public class TextInputSourceTest
+    {
+        [Fact]
+        public void GetData_WithData_Assertive() 
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //Arrange
+                object[] expected = new object[] { "Column 1", "Column 2", "Column 3" };
+                string records = "Column 1;Column 2;Column 3";
+
+                StreamWriter sw = new StreamWriter(ms);
+                sw.WriteLine(records);
+                sw.Flush();
+                ms.Position = 0;
+
+                //Act
+                StreamReader sr = new StreamReader(ms);
+                TextInputSource source = new TextInputSource(sr);
+
+                //Assert
+                Assert.True(source.Next());
+                Assert.Equal(expected, source.GetData());
+            }
+        }
+    }
+}
--- tests/Circle.Core.Tests/DataTransformer/Outputs/Sources/TextOutputSourceTest.cs
+++ tests/Circle.Core.Tests/DataTransformer/Outputs/Sources/TextOutputSourceTest.cs
@@ -9,23 +9,27 @@ namespace Circle.Core.Tests.DataTransformer.Outputs.Sources
         [Fact]
         public void SetData_WithRecords_Assertive()
         {
-            //Arrange
-            string[] records = new string[] { "Column 1", "Column 2", "Column 3"};
-            string expected = "Column 1;Column 2;Column 3";
-            string filename = "./TextOutputSourceTest_SetData_WithRecords_Assertive";
+            using (MemoryStream stream = new MemoryStream()) 
+            { 
+                //Arrange
+                string[] records = new string[] { "Column 1", "Column 2", "Column 3" };
+                string expected = "Column 1;Column 2;Column 3";
 
-            //Act
-            TextOutputSource outputSource = new TextOutputSource();
-            outputSource.Open(filename);
-            outputSource.SetData(records);
-            outputSource.Close();
-            
-            StreamReader reader = new StreamReader(filename);
-            string current = reader.ReadLine();
-            reader.Close();
-            
-            //Assert
-            Assert.Equal(expected, current);
+                //Act
+                StreamWriter writer = new StreamWriter(stream);
+                TextOutputSource outputSource = new TextOutputSource(writer);
+                outputSource.SetData(records);
+                
+                writer.Flush();
+                stream.Position = 0;
+
+                //Assert
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string current = reader.ReadLine();
+                    Assert.Equal(expected, current);
+                }
+            }
         }
     }
 }
